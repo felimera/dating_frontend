e4b43cd3c773710074c12b1ae1553138013690dc
andres.mera <feli041089@gmail.com>
2023-10-31 09:27:02 -0500

    dev-arquict-hex : Se creo en alchivo de implementacion del repository AssignmentRepositoryImpl con sus metodo.

---
+++ src/app/infrastructure/repositories/assignment.repository.impl.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Assignment } from 'src/app/core/models/assignment.model';
+import { AssignmentRepository } from 'src/app/core/repositories/assignment.repository';
+import {AssignmentService} from '../services/assignment.service';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+ })
+ export class AssignmentRepositoryImpl implements AssignmentRepository {
+  constructor(private assignmentService: AssignmentService) {}
+
+  getAll(): Observable<Assignment[]> {
+    return this.assignmentService.getAll();
+  }
+}
