7f2e2e673a4cd16f7a68cd4df43c5c0a8db86d80
andres.mera <feli041089@gmail.com>
2023-10-31 09:27:42 -0500

    dev-arquict-hex : Se creo en alchivo el archivo de la clase AssignmentService.

---
+++ src/app/infrastructure/services/assignment.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AssignmentMapper } from 'src/app/core/mappers/assignment.mapper';
+import { Assignment } from 'src/app/core/models/assignment.model';
+import { AssignmentDTO } from '../dto/assignment.dto';
+import { Observable, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AssignmentService {
+  private apiUrl = 'http://localhost:82/api/assignment/all';
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Assignment[]> {
+    return this.http
+      .get<AssignmentDTO[]>(this.apiUrl)
+      .pipe(map((apiAssignments) => apiAssignments.map(AssignmentMapper.fromApiToDomain)));
+  }
+}
