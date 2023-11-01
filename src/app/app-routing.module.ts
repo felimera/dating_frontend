3aa61212453d187584c25833b55f23d5c16fde20
andres.mera <feli041089@gmail.com>
2023-11-01 08:41:24 -0500

    dev-header : Agregando el componente al menu lateral.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListAssignmentComponent } from './modules/assignment/component/list-assignment/list-assignment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: ListAssignmentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
