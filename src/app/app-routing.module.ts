b5adca7f69abb9b0aa85904de29a56273548f844
andres.mera <feli041089@gmail.com>
2023-11-02 07:18:36 -0500

    dev-token : Agregando el component de login al panel lateral.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListAssignmentComponent } from './modules/assignment/component/list-assignment/list-assignment.component';
+import { UserLoginComponent } from './modules/user/component/user-login/user-login.component';
 
 const routes: Routes = [
-  { path: "home", component: ListAssignmentComponent }
+  { path: "home", component: ListAssignmentComponent },
+  { path: "login", component: UserLoginComponent }
 ];
 
 @NgModule({
