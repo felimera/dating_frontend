d4a38d56893dbf0c254fd1b88b28f9c489a71a05
andres.mera <feli041089@gmail.com>
2023-11-03 10:40:32 -0500

    dev-token : Consultando desde el backend y agregado token al localstorage.

--- src/app/modules/user/component/user-login/user-login.component.ts
+++ src/app/modules/user/component/user-login/user-login.component.ts
@@ -1,36 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { UserTokenService } from 'src/app/infrastructure/services/user-token.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
   styleUrls: ['./user-login.component.css'],
 })
-export class UserLoginComponent implements OnInit{
+export class UserLoginComponent implements OnInit {
 
-  userTokenForm!:FormGroup ;
+  userTokenForm!: FormGroup;
   hide = true;
 
+  constructor(private userLogin: UserTokenService, private router: Router) { }
+
   ngOnInit() {
-    this.userTokenForm=new FormGroup({
-      email:new FormControl('',[Validators.required, Validators.email]),
-      password:new FormControl('')
+    this.userTokenForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('')
     });
   }
 
-  getErrorMessage():string {
-    if (this.userTokenForm.valid&&this.userTokenForm.get('email')!.value.hasError('required')) {
+  getErrorMessage(): string {
+    if (this.userTokenForm.valid && this.userTokenForm.get('email')!.value.hasError('required')) {
       return 'You must enter a value';
     }
-
-    return this.userTokenForm.valid&& this.userTokenForm.get('email')!.value.hasError('email') ? 'Not a valid email' : '';
+    return this.userTokenForm.valid && this.userTokenForm.get('email')!.value.hasError('email') ? 'Not a valid email' : '';
   }
 
-  login():void
-  {
-
+  onLogin(): void {
+    this.userLogin.postToken(this.userTokenForm.value).subscribe((res: any) => {
+      localStorage.setItem('TOKEN', res.jwtToken);
+      this.router.navigateByUrl('/');
+    }, error => console.error('error', error));
   }
 }
