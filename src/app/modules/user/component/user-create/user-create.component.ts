83703ab86f1e3781c41228df832e65b0a85b284a
andres.mera <feli041089@gmail.com>
2023-11-08 08:57:54 -0500

    de-token : Obteniendo los datos para enviarlo al backend.

--- src/app/modules/user/component/user-create/user-create.component.ts
+++ src/app/modules/user/component/user-create/user-create.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-create',
   templateUrl: './user-create.component.html',
   styleUrls: ['./user-create.component.css']
 })
-export class UserCreateComponent {
+export class UserCreateComponent implements OnInit {
+  userForm!: FormGroup;
 
+  hide = true;
+  ngOnInit(): void {
+    this.userForm = new FormGroup({
+      nombres: new FormControl(''),
+      firt_name: new FormControl(''),
+      last_name: new FormControl(''),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+      verify_password: new FormControl('', [Validators.required]),
+      telefono: new FormControl('', [Validators.required]),
+      rol: new FormControl('', [Validators.required]),
+      genero: new FormControl('', [Validators.required]),
+      fechaNacimiento: new FormControl('', [Validators.required]),
+    });
+
+
+    //Not sure if Angular unsubscribes to this, so best to do it yourself
+    this.userForm.valueChanges.subscribe((change) => {
+      if (change.password !== change.verify_password) {
+        this.userForm.get('verify_password')!.setErrors({ isError: true });
+      } else if (change.verify_password === '') {
+        //this is needed in case the user empties both fields, else it would
+        //say they matched and therefore it's valid - the custom validator will
+        //not pick up on this edge case
+        this.userForm.get('verify_password')!.setErrors({ isError: true });
+      } else if (change.password === change.verify_password) {
+        //this removes the previously set errors
+        this.userForm.get('verify_password')!.setErrors(null);
+      }
+    });
+  }
+
+  getErrorMessage(): string {
+    if (this.userForm.valid && this.userForm.get('email')!.value.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.userForm.valid && this.userForm.get('email')!.value.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  onCreater(): void {
+    const nombres = this.userForm.get('firt_name')?.value + '&' + this.userForm.get('last_name')?.value;
+    this.userForm.get('nombres')?.setValue(nombres);
+    this.userForm.get('rol')?.setValue('U');
+
+    console.log('this.userForm.value', this.userForm.value);
+  }
 }
