c3c10d620813c9edf8cada38523fadf5f6ec0cbf
andres.mera <feli041089@gmail.com>
2023-11-17 09:02:32 -0500

    dev-user : Rectificando el inicio de sesion y cirre de sesion.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { CustomerDTO } from './infrastructure/dto/customer.dto';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dating_frontend';
   opened = false;
+
+  customer: CustomerDTO | any;
+
+  constructor(private cookieService: CookieService) { }
+
+  ngOnInit(): void {
+    this.isExistUser();
+  }
+
+  isExistUser(): boolean {
+    return (this.cookieService.check('usuario') && localStorage.getItem('TOKEN')!.length > 0)
+  }
+
+  validarTipoUsuario(): boolean {
+    if (this.isExistUser()) {
+      this.customer = JSON.parse(this.cookieService.get('usuario'));
+      const tipoUsuario = this.customer.rol;
+      console.log(tipoUsuario[0])
+      return tipoUsuario[0] === 'A';
+    }
+    return false;
+  }
+
+  cerrarSesion(): void {
+    this.cookieService.delete('usuario');
+    localStorage.removeItem('TOKEN')
+  }
 }
