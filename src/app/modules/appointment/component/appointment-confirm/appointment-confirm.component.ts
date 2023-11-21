4bcd2674b5e1d795518fe2471d1da60911ee2e57
andres.mera <feli041089@gmail.com>
2023-11-21 09:20:14 -0500

    dev-appcofirm : Acomodando la tabla para mostrar los datos del endpoint.

--- src/app/modules/appointment/component/appointment-confirm/appointment-confirm.component.ts
+++ src/app/modules/appointment/component/appointment-confirm/appointment-confirm.component.ts
@@ -1,33 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
 import { Router } from '@angular/router';
-import { AppointmentService } from 'src/app/infrastructure/services/appointment.service';
 import { CookieService } from 'ngx-cookie-service';
-import { AppointmentDTO } from 'src/app/infrastructure/dto/appointment.dto';
 import { CustomerDTO } from 'src/app/infrastructure/dto/customer.dto';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-// const ELEMENT_DATA: PeriodicElement[] = [
-//   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-//   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-//   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-//   {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-//   {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-//   {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-//   {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-//   {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-//   {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-//   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-// ];
-
-const ELEMENT_DATA: AppointmentDTO[] = [];
-
+import { AppointmentTableService } from 'src/app/infrastructure/services/appointment-table.service';
+import { AppointmentTable } from 'src/app/core/models/appointment-table.model';
+import { AppointmentTableDTO } from 'src/app/infrastructure/dto/appointment-table.dto';
+import { ContentTableDTO } from 'src/app/infrastructure/dto/content-table.dto';
 
 @Component({
   selector: 'app-appointment-confirm',
@@ -36,28 +14,50 @@ const ELEMENT_DATA: AppointmentDTO[] = [];
 })
 export class AppointmentConfirmComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  dataAppTableDTO: AppointmentTableDTO[] = [];
+  dataTableDTO: ContentTableDTO[] = [];
+
+  displayedColumns: string[] = ['name', 'description', 'price', 'symbol'];
+  dataSource: any;
+
+  panelOpenState = false;
 
   custumer: CustomerDTO | undefined;
 
   constructor(
     private router: Router,
-    private appointmentService:AppointmentService,
+    private appointmentTableService: AppointmentTableService,
     private cookieService: CookieService
   ) { }
 
   ngOnInit(): void {
     this.custumer = JSON.parse(this.cookieService.get('usuario'));
 
-    this.appointmentService
-    .getAppointmentByIdCustomer(this.custumer!.id)
-    .subscribe({
-      next:
-        (res:any)=>{
-          console.log("res",res);
-        }
-    })
+    this.loadDataIntoTable();
+  }
+
+  loadDataIntoTable(): void {
+    this.appointmentTableService
+      .getAppointmentByIdCustomer(this.custumer!.id)
+      .subscribe({
+        next:
+          (appTable: AppointmentTable[]) => {
+            appTable.forEach(app => {
+              this.dataTableDTO = [];
+              app.contentTableList.forEach(element => {
+                this.dataTableDTO.push({ idAssignment: element.idAssignment, nombre: element.nombre, descripcion: element.descripcion, precio: element.precio })
+              })
+              this.dataAppTableDTO.push({ idAppointment: app.idAppointment, fecha: app.fecha, precioTotal: app.precioTotal, contentTableList: this.dataTableDTO });
+            })
+            this.dataSource = this.dataAppTableDTO;
+          },
+        error: error => console.log(error)
+      })
+  }
+
+  isExitData(): boolean {
+    console.log(this.dataAppTableDTO)
+    return this.dataAppTableDTO.length > 0;
   }
 
   onReturnHome(): void {
