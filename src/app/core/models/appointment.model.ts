08b9a2953690d20570ad91402524c27fe3d09d33
andres.mera <feli041089@gmail.com>
2023-11-23 14:30:48 -0500

    dev-appconfirm : Modificacndo la prioridad de los atributos del cuerpo.

--- src/app/core/models/appointment.model.ts
+++ src/app/core/models/appointment.model.ts
@@ -1,11 +1,11 @@
 export class Appointment {
   constructor(
-    public id: number,
-    public fecha: string,
-    public hora: string,
-    public precioTotal: number,
-    public idCustomer: number,
-    public idAssignment: number,
-    public idsAssignment: number[],
+    public id?: number,
+    public fecha?: string,
+    public hora?: string,
+    public precioTotal?: number,
+    public idCustomer?: number,
+    public idAssignment?: number,
+    public idsAssignment?: number[],
   ) { }
 }
