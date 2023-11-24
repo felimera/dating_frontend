de811b2adfb96e7821ef9093b0f9d3dc7e3eef10
andres.mera <feli041089@gmail.com>
2023-11-24 11:10:10 -0500

    dev-user : Creo la funcionalidad de edicion del perfil de un usuario.

--- src/app/modules/user/component/user-edit/user-edit.component.ts
+++ src/app/modules/user/component/user-edit/user-edit.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { CookieService } from 'ngx-cookie-service';
 import { Customer } from 'src/app/core/models/customer.model';
-import { UserSignup } from 'src/app/core/models/user-signup.model';
 import { CustomerDTO } from 'src/app/infrastructure/dto/customer.dto';
 import { DatePipe } from '@angular/common';
 import { CustomerService } from 'src/app/infrastructure/services/customer.service';
+import { ToasterService } from 'src/app/infrastructure/services/generally/toaster.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-edit',
@@ -14,27 +15,30 @@ import { CustomerService } from 'src/app/infrastructure/services/customer.servic
 })
 export class UserEditComponent implements OnInit {
   userForm!: FormGroup;
+  usuario: CustomerDTO | undefined;
   hide = true;
 
   pipe = new DatePipe('en-US');
 
   constructor(
     private cookieService: CookieService,
-    private customerService: CustomerService
+    private customerService: CustomerService,
+    private toasterService: ToasterService,
+    private router: Router
   ) { }
-  
+
   ngOnInit(): void {
-    const usuario: CustomerDTO = JSON.parse(this.cookieService.get('usuario'));
-    const fechaFormateada = this.pipe.transform(usuario.fechaNacimiento, 'MM-dd-yyyy');
+    this.usuario = JSON.parse(this.cookieService.get('usuario'));
+    const fechaFormateada = this.pipe.transform(this.usuario!.fechaNacimiento, 'MM-dd-yyyy');
 
     this.userForm = new FormGroup({
-      nombre: new FormControl(usuario.nombre, [Validators.required]),
-      apellido: new FormControl(usuario.apellido, [Validators.required]),
-      genero: new FormControl(usuario.genero[0], [Validators.required]),
+      nombre: new FormControl(this.usuario!.nombre, [Validators.required]),
+      apellido: new FormControl(this.usuario!.apellido, [Validators.required]),
+      genero: new FormControl(this.usuario!.genero[0], [Validators.required]),
       fechaNacimiento: new FormControl(new Date(fechaFormateada!), [Validators.required]),
-      telefono: new FormControl(usuario.telefono, [Validators.required]),
-      correo: new FormControl(usuario.correo, [Validators.required, Validators.email]),
-      rol: new FormControl(usuario.rol, [Validators.required])
+      telefono: new FormControl(this.usuario!.telefono, [Validators.required]),
+      correo: new FormControl(this.usuario!.correo, [Validators.required, Validators.email]),
+      rol: new FormControl(this.usuario!.rol[0], [Validators.required])
     });
   }
 
@@ -46,6 +50,24 @@ export class UserEditComponent implements OnInit {
   }
 
   onEditUser(): void {
-    throw new Error('Method not implemented.');
+    const fechaFormateada = this.pipe.transform(this.userForm.get('fechaNacimiento')?.value, 'dd/MM/yyyy');
+    this.userForm.get('fechaNacimiento')?.setValue(fechaFormateada);
+
+    this.customerService
+      .putCustomer(this.usuario!.id, this.userForm.value)
+      .subscribe({
+        next: (res: Customer) => {
+          if (res) {
+            this.cookieService.set('usuario', JSON.stringify(res));
+            this.toasterService.info('Usuario editado con exito.', 'Customer edit');
+          }
+        },
+        error: error => console.log(error)
+
+      });
+  }
+
+  onCancel(): void {
+    this.router.navigateByUrl('/home');
   }
 }
