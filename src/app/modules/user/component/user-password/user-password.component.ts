17e1a3f3e6c7e59b497cf819331f21b1fb38a27e
andres.mera <feli041089@gmail.com>
2023-11-27 07:59:05 -0500

    dev-user : Agregando la plantilla con sus estilos. Y algo del cuerpo

--- src/app/modules/user/component/user-password/user-password.component.ts
+++ src/app/modules/user/component/user-password/user-password.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-password',
   templateUrl: './user-password.component.html',
   styleUrls: ['./user-password.component.css']
 })
-export class UserPasswordComponent {
+export class UserPasswordComponent implements OnInit {
 
+  userForm!: FormGroup;
+  hide = true;
+
+  ngOnInit(): void {
+
+    this.userForm = new FormGroup({
+      password_old: new FormControl('', [Validators.required]),
+      password_new: new FormControl('', [Validators.required]),
+      verify_password_new: new FormControl('', [Validators.required])
+    });
+
+    //Not sure if Angular unsubscribes to this, so best to do it yourself
+    this.userForm.valueChanges.subscribe((change) => {
+      if (change.password !== change.verify_password) {
+        this.userForm.get('verify_password')!.setErrors({ isError: true });
+      } else if (change.verify_password === '') {
+        //this is needed in case the user empties both fields, else it would
+        //say they matched and therefore it's valid - the custom validator will
+        //not pick up on this edge case
+        this.userForm.get('verify_password')!.setErrors({ isError: true });
+      } else if (change.password === change.verify_password) {
+        //this removes the previously set errors
+        this.userForm.get('verify_password')!.setErrors(null);
+      }
+    });
+  }
+
+  onChangePassword(): void {
+
+  }
 }
