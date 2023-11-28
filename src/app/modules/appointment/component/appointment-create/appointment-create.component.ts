f37769579b6464de4aefc351d82fef94e61b02f2
andres.mera <feli041089@gmail.com>
2023-11-28 10:38:50 -0500

    dev : Mejorando el codigo de la creacion del appointment.

--- src/app/modules/appointment/component/appointment-create/appointment-create.component.ts
+++ src/app/modules/appointment/component/appointment-create/appointment-create.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { DateAdapter } from '@angular/material/core';
 import { DatePipe } from '@angular/common';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { AssignmentService } from 'src/app/infrastructure/services/assignment.service';
 import { AssignmentDTO } from 'src/app/infrastructure/dto/assignment.dto';
 import { CustomerDTO } from 'src/app/infrastructure/dto/customer.dto';
 import { CookieService } from 'ngx-cookie-service';
 import { AppointmentService } from 'src/app/infrastructure/services/appointment.service';
 import { ToasterService } from 'src/app/infrastructure/services/generally/toaster.service';
+import { Assignment } from 'src/app/core/models/assignment.model';
 
 @Component({
   selector: 'app-appointment-create',
@@ -36,30 +37,46 @@ export class AppointmentCreateComponent implements OnInit {
     private assingmentService: AssignmentService,
     private cookieService: CookieService,
     private appointmentService: AppointmentService,
-    private toasterService: ToasterService
+    private toasterService: ToasterService,
+    private router: Router
   ) {
     this.dateAdapter.setLocale('Es');
   }
 
   ngOnInit(): void {
 
-    this.activatedRoute.queryParams.subscribe(params => {
-      this.assingmentService.getByListIds(params['ids']).subscribe(res => {
-        this.assingments = res;
-        this.priceTotal = this.assingments.map(t => t.precio).reduce((acc, value) => value + acc, 0);
-      })
-    });
+    this.customer = JSON.parse(this.cookieService.get('usuario'));
 
     this.appointmentForm = new FormGroup({
       fecha: new FormControl('', [Validators.required]),
       hora: new FormControl('', [Validators.required]),
       precioTotal: new FormControl('', [Validators.required]),
-      idCustomer: new FormControl('', [Validators.required]),
-      idsAssignment: new FormControl([''], [Validators.required])
+      idCustomer: new FormControl(this.customer.id, [Validators.required]),
+      idsAssignment: new FormControl('', [Validators.required])
     });
 
-    this.customer = JSON.parse(this.cookieService.get('usuario'));
+    this.activatedRoute.queryParams.subscribe(params => {
+      if (params) {
+        this.consultarServicioAppointment(params);
+      }
+    });
+
     this.limitesFechaSeleccionada();
+  }
+
+  consultarServicioAppointment(params: Params): void {
+    this.assingmentService
+      .getByListIds(params['ids'])
+      .subscribe({
+        next: (res: Assignment[]) => {
+          if (res) {
+            this.assingments = res;
+            this.priceTotal = this.assingments.map(t => t.precio).reduce((acc, value) => value + acc, 0);
+            this.appointmentForm.get('idsAssignment').setValue(this.assingments.map(assignment => assignment.id));
+          }
+        },
+        error: res => console.log(res.error)
+      })
   }
 
   limitesFechaSeleccionada(): void {
@@ -81,14 +98,19 @@ export class AppointmentCreateComponent implements OnInit {
 
     this.appointmentForm.get('fecha')!.setValue(fechaFormateada);
     this.appointmentForm.get('precioTotal')!.setValue(this.priceTotal);
-    this.appointmentForm.get('idCustomer').setValue(this.customer.id);
-    this.appointmentForm.get('idsAssignment').setValue(this.assingments.map(assignment => assignment.id));
 
     this.appointmentService
       .postAppointment(this.appointmentForm.value)
       .subscribe({
         next: (res: any) => {
-          this.toasterService.success('Registro guardado exitosamente.', 'Appoinment create')
+          if (res) {
+            this.toasterService.success('Registro guardado exitosamente.', 'Appoinment create');
+            setTimeout(() => {
+              this.appointmentForm.get('fecha')!.setValue('');
+              this.appointmentForm.get('precioTotal')!.setValue('');
+              this.router.navigateByUrl("/home");
+            }, 3000);
+          }
         }, error: error => console.error('error', error)
       })
   }
