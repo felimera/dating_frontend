b414248673ad5758b3b63644bb7864b77870788e
andres.mera <feli041089@gmail.com>
2023-12-05 07:57:00 -0500

    dev-report : Generando el nuevo compoente para los reportes.

---
+++ src/app/modules/report/component/generate-report/generate-report.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generate-report',
+  templateUrl: './generate-report.component.html',
+  styleUrls: ['./generate-report.component.css']
+})
+export class GenerateReportComponent {
+
+}
